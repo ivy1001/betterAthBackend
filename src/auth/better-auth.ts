492093f9dbe6e9d00a845ff1e1066c733ec0cbc4
uwu <oumaiymafrikel@gmail.com>
2026-01-27 17:02:14 +0100

    auth

--- src/auth/better-auth.ts
+++ src/auth/better-auth.ts
@@ -1,19 +1,25 @@
 import { betterAuth } from "better-auth";
 import Database from "better-sqlite3";
-import type { Auth } from "better-auth";
 
 const db = new Database("./auth.db");
 
-export const auth: Auth = betterAuth({
-  baseURL: process.env.BACKEND_URL,
-  secret: process.env.BETTER_AUTH_SECRET,
-  database: db,
-  emailAndPassword: {
-    enabled: true,
-  },
+export const auth = betterAuth({
+  baseURL: process.env.BACKEND_URL!,
+  secret: process.env.BETTER_AUTH_SECRET!,
+  database: db as any,
+
+  emailAndPassword: { enabled: true },
+
   trustedOrigins: [process.env.FRONTEND_URL!],
-   cookies: {
-    sameSite: "none",
-    secure: true,
+
+  advanced: {
+    cookies: {
+      sessionToken: {
+        attributes: {
+          sameSite: "none",
+          secure: true,
+        },
+      },
+    },
   },
-}) as Auth;
+});
